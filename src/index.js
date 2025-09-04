b33af9c28a8e0de327d57b038890d286dacf79c7
Br0cod3 <thespammingbott@gmail.com>
2025-09-04 07:40:18 +0100

    implement: clickBtn, firstTime, removePreviousTab functions and also added event listeners to the sections

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
 import "./styles.css"
 import { Home } from "./Home.js"
+import { Menu } from "./Menu.js"
+import { About } from "./About.js"
+import { Contact } from "./Contact.js"
 
-Home()
+const home = document.getElementById("home")
+const menu = document.getElementById("menu")
+const about = document.getElementById("about")
+const contact = document.getElementById("contact")
+
+const modules = {
+    Home,
+    Menu,
+    About,
+    Contact,
+}
+const buttons = [home, menu, about, contact]
+buttons.forEach(btn => {
+    btn.addEventListener("click", clickBtn)
+})
+
+function clickBtn(e) {
+    removePreviousTab()
+    const btnClicked = e.target.textContent
+    const title = `${btnClicked.slice(0, 1).toUpperCase()}${btnClicked.slice(1).toLowerCase()}`
+    for (const [key, value] of Object.entries(modules)) {
+        if (title === key) {
+            value()
+        }
+    }
+    return;
+
+}
+
+function removePreviousTab() {
+    const container = document.getElementById("content")
+    container.innerHTML = ""
+    container.style = ""
+    container.classList = ""
+}
+
+function firstTime() {
+    let check = 0
+    if (check < 1) Home()
+    check++
+}
+
+firstTime()
